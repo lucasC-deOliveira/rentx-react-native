da278f0293721846cb7213dcd50542d0371df3e9
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-07-31 18:36:28 -0300

    Tema Global da Aplicação

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 import { Home } from './src/screens/Home';
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from 'styled-components'
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from "@expo-google-fonts/inter"
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from "@expo-google-fonts/archivo"
+import theme from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  })
+
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   )
 
 }
