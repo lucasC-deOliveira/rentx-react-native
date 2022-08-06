17098608fccc51d413f47842f6308659cd9e527f
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-08-06 15:56:02 -0300

    Componente de Acessórios

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Accessory } from '../../components/Accessory';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+
+import SpeedSvg from "../../assets/speed.svg"
+import AccelerationSvg from "../../assets/acceleration.svg"
+import ForceSvg from "../../assets/force.svg"
+import GasolineSvg from "../../assets/gasoline.svg"
+import ExchangeSvg from "../../assets/exchange.svg"
+import PeopleSvg from "../../assets/people.svg"
 
 import {
   Container,
@@ -15,6 +23,7 @@ import {
   Period,
   Price,
   About,
+  Accessories,
 } from './styles'
 
 export function CarDetails() {
@@ -38,9 +47,17 @@ export function CarDetails() {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
+        <Accessories>
+          <Accessory name="380Km/h"  icon={SpeedSvg} />
+          <Accessory name="3.2s"  icon={AccelerationSvg} />
+          <Accessory name="800 Hp"  icon={ForceSvg} />
+          <Accessory name="Gasolina"  icon={GasolineSvg} />
+          <Accessory name="Auto"  icon={ExchangeSvg} />
+          <Accessory name="2 pessoas"  icon={PeopleSvg} />
+        </Accessories>
         <About>
-          Este é um automovel desportivo. surgiu do lendario 
-          touro de lide indultado na praça Real Maestranza de Sevilla. 
+          Este é um automovel desportivo. surgiu do lendario
+          touro de lide indultado na praça Real Maestranza de Sevilla.
           É um belissimo carro para quem gosta de acelerar
         </About>
       </Content>
