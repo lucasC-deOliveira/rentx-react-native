b8f4bd3455124b8102b4fb680f5c2a17125a013a
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-08-07 19:07:44 -0300

    Melhorando o Scroll

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Accessory } from '../../components/Accessory';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
 import { getAccessoryIcon } from "../../Utils/getAccessoryIcon"
 import { Button } from '../../components/Button';
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -32,6 +32,7 @@ import {
   Accessories,
   Footer,
 } from './styles'
+import { useTheme } from 'styled-components';
 
 
 interface Params {
@@ -45,6 +46,8 @@ export function CarDetails() {
 
   const { car } = route.params as Params;
 
+  const theme = useTheme()
+
   const scrollY = useSharedValue(0);
 
   const scrollHandler = useAnimatedScrollHandler(event => {
@@ -57,6 +60,17 @@ export function CarDetails() {
         scrollY.value,
         [0, 200],
         [200, 70],
+        Extrapolate.CLAMP
+      )
+    }
+  })
+
+  const sliderCarsStyleAnimation = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        scrollY.value,
+        [0, 150],
+        [1, 0],
         Extrapolate.CLAMP
       )
     }
@@ -80,23 +94,31 @@ export function CarDetails() {
         backgroundColor="transparent"
       />
       <Animated.View
-      style={[headerStyleAnimation]}
+        style={[headerStyleAnimation,
+           styles.header,
+           {backgroundColor: theme.colors.background_secondary}
+          ]}
       >
         <Header>
-          <BackButton onPress={handleBack} />
+          <BackButton onPress={handleBack}  />
         </Header>
-        <CarImages>
-          <ImageSlider
-            imagesUrl={car.photos} />
-        </CarImages>
+        <Animated.View
+          style={[sliderCarsStyleAnimation]}
+        >
+          <CarImages>
+            <ImageSlider
+              imagesUrl={car.photos} />
+          </CarImages>
+        </Animated.View>
       </Animated.View>
       <Animated.ScrollView
         contentContainerStyle={{
           paddingHorizontal: 24,
-          paddingTop: getStatusBarHeight()
+          paddingTop: getStatusBarHeight() + 160
         }}
         showsVerticalScrollIndicator={false}
         onScroll={scrollHandler}
+        scrollEventThrottle={16}
       >
         <Details>
           <Description>
@@ -138,3 +160,11 @@ export function CarDetails() {
     </Container >
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    position: "absolute",
+    overflow: "hidden",
+    zIndex: 1
+  },
+})
