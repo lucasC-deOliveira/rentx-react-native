bb725d30eb0cce80dea5467ccd1aeaac0e3814aa
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-08-07 16:31:38 -0300

    Obtendo a imagem em foco

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { ViewToken } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
 import {
@@ -14,14 +15,27 @@ interface Props {
   imagesUrl: string[]
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrl }: Props) {
+
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+      const index = info.viewableItems[0].index!
+      setImageIndex(index)
+  })
+
   return (
     <Container>
       <ImageIndexes>
         {imagesUrl.map((_, index) => (
           <ImageIndex
             key={String(index)}
-            active={true}
+            active={index === imageIndex}
           />
         ))
         }
@@ -41,6 +55,7 @@ export function ImageSlider({ imagesUrl }: Props) {
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged.current}
       />
 
 
