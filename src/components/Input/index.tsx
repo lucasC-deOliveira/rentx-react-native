889b3df7f2977450c1fe0f2d82ebb83a265b7452
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-08-07 21:46:19 -0300

    Input de Email

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,21 +5,27 @@ import { TextInputProps } from 'react-native';
 
 import {
   Container,
+  IconContainer,
+  InputText
 } from './styles'
 
 interface InputProps extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>["name"]
 }
 
-export function Input({ iconName }: InputProps) {
+export function Input({ iconName,...rest }: InputProps) {
   const theme = useTheme()
   return (
-    <Container>
-      <Feather
-        name={iconName}
-        size={24}
-        color={theme.colors.text_detail}
-      />
+    <Container >
+      <IconContainer>
+        <Feather
+          name={iconName}
+          size={24}
+          color={theme.colors.text_detail}
+        />
+      </IconContainer>
+
+      <InputText {...rest} />
     </Container>
   );
 }
