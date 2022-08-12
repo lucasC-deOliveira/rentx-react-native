4796fab74dad71cdedf0f3691ca284c258fd6e48
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-08-12 16:30:08 -0300

    Customizando o Bottom Tab

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -5,15 +5,45 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { AppStackRoutes } from "./app.stack.routes"
 const { Navigator, Screen } = createBottomTabNavigator()
 
+import HomeSvg from "../assets/home.svg"
+import CarSvg from "../assets/car.svg"
+import PeopleSvg from "../assets/people.svg"
+import { useTheme } from "styled-components"
+import { Platform } from "react-native"
+
 export function AppTabRoutes() {
+  const theme = useTheme()
   return (
-    <Navigator 
-    screenOptions={{ headerShown: false }}
+    <Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.main,
+        tabBarInactiveTintColor: theme.colors.text_detail,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 78,
+          backgroundColor: theme.colors.background_primary
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === 'Home') {
+            return (<HomeSvg fill={color} width={24} height={24} />)
+          }
+          if (route.name === 'Profile') {
+            return (<PeopleSvg fill={color} width={24} height={24}  />)
+          }
+          if(route.name === 'MyCars'){
+            return (<CarSvg fill={color} width={24} height={24}  />)
+          }
+        }
+      })}
+
     >
 
       <Screen
         name="Home"
         component={AppStackRoutes}
+
       />
       <Screen
         name="Profile"
