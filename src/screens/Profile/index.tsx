5e6d132e6f456757c16fa5862ed46f6ad78983bd
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-08-13 16:57:59 -0300

    Confirmação de Sign out

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -114,6 +114,21 @@ export function Profile() {
     }
   }
 
+  async function handleSignOut() {
+    Alert.alert('Tem certeza?',
+      'Lembre-se que se você sair, irá precisar de internet para conectar-se novamente.',
+      [
+        {
+          text:"Cancelar",
+          onPress:()=>{}
+        },
+        {
+          text:"Sair",
+          onPress:signOut
+        }
+      ])
+  }
+
   return (
     <KeyboardAvoidingView behavior='position' enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -125,7 +140,7 @@ export function Profile() {
                 onPress={handleBack}
               />
               <HeaderTitle>Editar Perfil</HeaderTitle>
-              <LogoutButton onPress={signOut}>
+              <LogoutButton onPress={handleSignOut}>
                 <Feather
                   name="power"
                   size={24}
